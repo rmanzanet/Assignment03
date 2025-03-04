831b0a1d46b927b1e558d4cc91b47789771af9d3
rmanzanet <ridder.manzanet@gmail.com>
2025-03-03 17:36:41 -0800

    Create loopingTriangle.js

---
+++ js/loopingTriangle.js
@@ -0,0 +1,8 @@
+// Initialize an empty string
+let triangleBuilt = "";
+
+// Loop to generate the pattern
+for (let i = 1; i <= 7; i++) {
+    triangleBuilt += "#";  // Add a "#" each iteration
+    console.log(triangleBuilt); // Print the current line
+}
