b343416200c3c2fe8e2d3a313db3e9ec1c0af759
rmanzanet <ridder.manzanet@gmail.com>
2025-03-03 17:42:08 -0800

    Update 05-marco-polo.js

--- js/05-marco-polo.js
+++ js/05-marco-polo.js
@@ -1,1 +1,13 @@
-
+// Loop from 1 to 100
+for (let i = 1; i <= 100; i++) {
+    // Check for multiples of both 3 and 5 first
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log("Marco! Polo!");
+    } else if (i % 3 === 0) { // Check for multiples of 3
+        console.log("Marco!");
+    } else if (i % 5 === 0) { // Check for multiples of 5
+        console.log("Polo!");
+    } else { // Print the number if none of the above conditions are met
+        console.log(i);
+    }
+}
